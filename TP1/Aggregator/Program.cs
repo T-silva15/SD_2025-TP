962dff9279191899d7e7916c4bf44b41dd9a18ca
Tiago Silva <tiagoissilva@hotmail.com>
2025-03-18 19:50:39 +0000

    wavy data generation

--- TP1/Aggregator/Program.cs
+++ TP1/Aggregator/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+
+class Aggregator
+{
+	
+}
+
+
--- TP1/Wavy/Program.cs
+++ TP1/Wavy/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Wavy
+{
+	public static void Main(string[] args)
+	{
+		while (true)
+		{
+			Thread.Sleep(10000);
+			var freq = GenerateTemp(108, 0);
+			Console.WriteLine($"Frequency: {freq}Hz");
+			Thread.Sleep(10000);
+			var db = GenerateTemp(150, 0);
+			Console.WriteLine($"Decibels: {db}dB");
+			Thread.Sleep(10000);
+			var wind = GenerateTemp(300, 0);
+			Console.WriteLine($"Wind Speed: {wind}km/h");
+			Thread.Sleep(30000);
+			var temp = GenerateTemp(40, 0);
+
+			Console.WriteLine($"Temperature: {temp}°C");
+		}
+	}
+
+	public static float GenerateTemp(int max, int min)
+	{		
+		Random random = new Random();
+		var x = (float)random.NextDouble() * (max - min) + min;
+		return x;
+	}
+
+
+}
